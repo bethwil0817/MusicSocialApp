add9578ef519da2ecc51e32241a17a28c06ae572
bethwillner <156834203+bethwillner@users.noreply.github.com>
2026-05-21 19:53:19 -0500

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as styles from '../styles/App.module.css';
+
+export default function App() {
+    return (
+        <div className="center">
+            <h1 className={styles.cursive}>Hello World</h1>
+        </div>
+    )
+}
